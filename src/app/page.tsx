48bb3f31e2e5205eecb167cc8f15e24e0a65809d
juankm08-a11y <juancarlospabon01@gmail.com>
2025-03-12 07:50:07 -0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center w-screen h-screen bg-green-200">
+      <h1 className="text-blue-900 font-extrabold text-4xl">
+        Juan Carlos Muñoz Pabon
+      </h1>
+    </div>
+  );
+}
