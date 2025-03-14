f48e88719dad3d9f3ef0c2ac1ffe2052d5837fba
juankm08-a11y <juancarlospabon01@gmail.com>
2025-03-14 12:33:53 -0500

    third  commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Portafolio Juan",
+  title: "Portafolio Juan - Desarrollador de Software & Ingenierio de Software",
   description: "Mi Portafolio",
 };
 
