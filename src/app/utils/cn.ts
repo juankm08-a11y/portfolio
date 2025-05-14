e10313b206f33aea962c21caea1bd13dcd6ff6a8
jhonkw9w8e <juan.munozpabon@campusucc.edu.co>
2025-05-14 07:13:37 -0500

    thirty-one commit

--- src/app/utils/cn.ts
+++ src/app/utils/cn.ts
@@ -1,8 +1,5 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-
-import React from "react";
-
 export function cn(...inputs: Parameters<typeof clsx>) {
   return twMerge(clsx(...inputs));
 }
