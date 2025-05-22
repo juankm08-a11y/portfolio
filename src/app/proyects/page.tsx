75f4fb5a39387a1f368ca0cefd488b3b26db6171
jhonkw9w8e <juan.munozpabon@campusucc.edu.co>
2025-05-22 13:59:37 -0500

    initial commit

--- src/app/proyects/page.tsx
+++ src/app/proyects/page.tsx
@@ -8,7 +8,7 @@ interface Proyect {
   title: string;
   description: string;
   imageUrl: string;
-  gitUrl: string;
+  url: string;
 }
 
 export default function ProyectsPage() {
@@ -34,22 +34,38 @@ export default function ProyectsPage() {
       title: "Scientific Calculator 2.0",
       description: "This my scientific Calculator version 2.0",
       imageUrl: "/images/proyects/scientific_calculator2.0.png",
-      gitUrl: "https://github.com/juankm08-a11y/Scientific-Calculator-2.0",
+      url: "https://github.com/juankm08-a11y/Scientific-Calculator-2.0",
     },
     {
       title: "Login Card",
       description: "This is Login Card",
       imageUrl: "/images/proyects/login_card.png",
-      gitUrl: "https://github.com/juankm08-a11y/Login-Card.git",
+      url: "https://github.com/juankm08-a11y/Login-Card.git",
     },
     {
-      title: "Login Card",
-      description: "This is Login Card",
-      imageUrl: "/images/proyects/.png",
-      gitUrl: "https://github.com/juankm08-a11y/my-music-player",
+      title: "Boarding Pass Card",
+      description: "This is my boarding pass card",
+      imageUrl: "/images/proyects/boardingpass.png",
+      url: "https://github.com/juankm08-a11y/my-music-player",
     },
-
-    /*Dynamic surveys is still a project in development and will be deployed on AWS.*/
+    {
+      title: "Music Player",
+      description: "This is music player in develop ",
+      imageUrl: "/images/proyects/mymusic_player.png",
+      url: "https://github.com/juankm08-a11y/my-music-player",
+    },
+    {
+      title: "Dynamic survey",
+      description: "This is a dynamic survey",
+      imageUrl: "/images/proyects/dinamicsurvey.png",
+      url: "https://mybucketencuestasdinamicas.s3.us-east-1.amazonaws.com/front/index.html",
+    },
+    {
+      title: "restaurants&reservations",
+      description: "This is software reservations&restaurants",
+      imageUrl: "/images/proyects/restaurants_reservations.png",
+      url: "https://github.com/juankm08-a11y/ReservasyRestaurantes.git",
+    },
   ];
 
   const toggleProyect = (index: number) => {
